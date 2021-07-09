43326f531b00848f739fde28726544e2f067b387
Josue Amador <bryan.amador@ucr.ac.cr>
2021-07-09 11:38:04 -0600

    Description added. Some styles updated.

--- header.js
+++ header.js
@@ -4,7 +4,7 @@
 window.onscroll = function () { setStickiness() };
 
 // Get the header
-var header = document.getElementById("myHeader");
+var header = document.getElementById("header");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
