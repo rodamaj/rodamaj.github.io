172823c5a123bbdd9a190063e9f0d7cab1d770fa
Josué Amador <54346349+rodamaj@users.noreply.github.com>
2021-07-29 22:46:54 -0600

    Language option added. Some styles changed.

---
+++ scripts/behavior.js
@@ -0,0 +1,24 @@
+let englishLink = document.getElementById("english-link");
+let spanishLink = document.getElementById("spanish-link");
+
+englishLink.onclick = function() {
+    toggleLink(englishLink, spanishLink, () => {
+        alert("Translation to English.");
+    });
+};
+
+spanishLink.onclick = function() {
+    toggleLink(spanishLink, englishLink, () => {
+        alert("Traducción a español.");
+    });
+};
+
+function toggleLink(clickedLink, newClickableLink, callback) {
+    if (newClickableLink.getAttribute("href") == null) {
+        clickedLink.removeAttribute("href");
+        clickedLink.style.fontWeight = "bold";
+        newClickableLink.setAttribute("href", "");
+        newClickableLink.style.fontWeight = "normal";
+        callback();
+    }
+}
